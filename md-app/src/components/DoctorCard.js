3aee842b1ba94e7bacf53228f44561e5a92fbc2e
Peter Fontana <dude0508@gmail.com>
2021-11-18 18:49:36 -0500

    added patient list to doctorcard

--- md-app/src/components/DoctorCard.js
+++ md-app/src/components/DoctorCard.js
@@ -1,7 +1,22 @@
 
+import { useEffect, useState } from "react";
 import DoctorForm from "./DoctorForm";
 
 function DoctorCard({doctor, setClicked, handleDoctorDelete, doctorData, handleDoctorForm, handleDoctorEdit, setEditing, setDoctorData, editing}) {
+    const [patients, setPatients] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:9292/doctors/${doctor.id}/patients`)
+        .then(r => r.json())
+        .then(data => {
+            setPatients(data)
+        })
+    })
+
+    let patientList = patients.map(patient => {
+        return <li key={patient.id} >{patient.first_name} {patient.last_name}</li>
+    })
+    
     return (
         < div style={doctorInfoCard}>
             {editing ? <DoctorForm doctorData={doctorData} handleDoctorForm={handleDoctorForm} handleDoctorSubmit={handleDoctorEdit} /> 
@@ -9,6 +24,10 @@ function DoctorCard({doctor, setClicked, handleDoctorDelete, doctorData, handleD
             <>
                 <h1>Dr. {doctor.first_name} {doctor.last_name}</h1>
                 <h2>Department: {doctor.specialty}</h2>
+                <h3>Patients:</h3>
+                <ul>
+                    {patientList.length === 0 ? <p>None</p> : patientList}
+                </ul>
                 <button onClick={() => {setEditing(true); setDoctorData(doctor)}}>Edit Doctor</button>
                 <button onClick={() => {handleDoctorDelete(doctor); setClicked(false)}}>Delete Doctor</button>
                 </>
